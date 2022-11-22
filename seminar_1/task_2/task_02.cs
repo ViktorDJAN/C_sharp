a6dd639c5e5f12901ba56e3143d35634672b8692
Viktor St.one <pistoletviktor@gmail.com>
2022-11-22 22:18:48 +0300

    Completed task two

---
+++ seminar_1/task_2/task_02.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+if(numDay == 1)  Console.WriteLine("Понедельник");
+if(numDay == 2)  Console.WriteLine("Вторник");
+if(numDay == 3)  Console.WriteLine("Среда");
+if(numDay == 4)  Console.WriteLine("Четверг");
+if(numDay == 5)  Console.WriteLine("Пятница");
+if(numDay == 6)  Console.WriteLine("Суббота");
+if(numDay == 7)  Console.WriteLine("Воскресенье");
