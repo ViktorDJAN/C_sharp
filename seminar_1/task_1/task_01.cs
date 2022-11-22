e0d8040708e1a1a73d3c8aa46909cc17383c1536
Viktor St.one <pistoletviktor@gmail.com>
2022-11-22 22:04:43 +0300

    completed task_01

---
+++ seminar_1/task_1/task_01.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+double numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double numberB = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(Math.Pow(number,2));
+if(numberA == Math.Pow(numberB,2))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
