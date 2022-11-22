c3562a39ef40029e26da3ebf99e1669844317909
Viktor St.one <pistoletviktor@gmail.com>
2022-11-22 22:59:07 +0300

    task_05 is completed

---
+++ seminar_1/task_5/task_05.cs
@@ -0,0 +1,11 @@
+﻿//Задача 5.
+//Напишите программу которая на вход принимает одно число N 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = -number; i < number + 1; i += 1)
+{
+    Console.Write(i + " ");
+
+} 
