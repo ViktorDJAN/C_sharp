a2ebba858650658964d790c679f350b3d5b3b54a
Viktor St.one <pistoletviktor@gmail.com>
2022-11-28 12:19:36 +0300

    task is completed

--- seminar_2/task_0/task_0.cs
+++ seminar_2/task_0/task_0.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая выводит случайное число из 
+// отрезка [10,99] и показывает наибольшую цифру числа.
+// например 78 -> 8  если 12 -> 2.
+
+int number = new Random().Next(10, 100);
+int digit2 = number % 10;
+int digit1 = number / 10;
+Console.WriteLine(number);
+
+if(digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+}
+else
+{
+   Console.WriteLine(digit2); 
+}
