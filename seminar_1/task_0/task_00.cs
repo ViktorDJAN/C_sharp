d239069bbc9f571d39cfd86ea1c742175daa7135
Viktor St.one <pistoletviktor@gmail.com>
2022-11-28 10:53:27 +0300

    some saving

--- seminar_1/task_0/task_00.cs
+++ seminar_1/task_0/task_00.cs
@@ -1,3 +1,16 @@
-﻿Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Math.Pow(number,2));
+﻿// применение индекса. usage index.
+int[] array = { 1, 23, 32, 24, 15, 64, 72, 89 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index += 1;
+}
