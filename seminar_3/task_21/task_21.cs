df9686b3c0141d48ff5b576b0c0fe4de1f8fa355
Viktor St.one <pistoletviktor@gmail.com>
2022-11-30 11:15:37 +0300

    task is competed

--- seminar_3/task_21/task_21.cs
+++ seminar_3/task_21/task_21.cs
@@ -1,4 +1,16 @@
 ﻿// Напишите программу которая находит расстояние между двумя координатными точками
 // в 2D пространстве.
 
-CoNSOLE
+Console.WriteLine("Введите перую координату Xa и Ya");
+double xa = Convert.ToInt32(Console.ReadLine());
+double ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую координату Xb и Yb");
+double xb = Convert.ToInt32(Console.ReadLine());
+double yb = Convert.ToInt32(Console.ReadLine());
+
+//double distant = Math.Sqrt((xa-xb) нахождение корня
+//Console.WriteLine(Math.Pow(xa, 2)) нахождение квадрата от числа
+
+double distant = Math.Sqrt(Math.Pow((xb-xa),2) + Math.Pow((yb - ya),2));
+Console.WriteLine(distant);
