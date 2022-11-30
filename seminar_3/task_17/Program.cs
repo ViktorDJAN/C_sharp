5baea0d0f878988c44fa10dfcf0d922d0ec03d81
Viktor St.one <pistoletviktor@gmail.com>
2022-11-30 10:05:24 +0300

    task about coordinats

--- seminar_3/task_17/Program.cs
+++ seminar_3/task_17/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая принимает на вход две координатные точки (Х и У) х не равен нули и у также не равен нулю
+// программа выдает номер четверти в которой находятся эта точка
+
+Console.WriteLine("Введите координату X ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
