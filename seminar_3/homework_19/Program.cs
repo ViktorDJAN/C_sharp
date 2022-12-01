7cfd71d1c92434ab798300dd026a5d79a809d4e4
Viktor St.one <pistoletviktor@gmail.com>
2022-12-01 10:14:37 +0300

    homework task 19

---
+++ seminar_3/homework_19/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу которая принимает на вход число N  и проверяет его на полиндромность
+// 23432 да   145668  нет
+
+
+int i = 0 , remainder = 0 ;
+string newnum ="";
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 0 ; i < number+1 ; i+= 1)
+for (i = number; i > 0; i = (i / 10))
+{
+    remainder = i % 10;
+    newnum += remainder;
+    
+}
+int NUM = Convert.ToInt32(newnum);
+if (number == NUM)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
