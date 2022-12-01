2f25a0235493892d668b45f989c17796b5934a13
Viktor St.one <pistoletviktor@gmail.com>
2022-12-01 10:28:27 +0300

    homework_23

---
+++ seminar_3/homework_23/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишите программу которая принимает на вход число N  и выдает таблицу кубов от 1 до N
+// 2    ---->  1 , 8....
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1 ; i < number + 1; i+= 1)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
