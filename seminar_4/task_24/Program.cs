954fd4456f2ced822ebcd43c61060f5f4b90998c
Viktor St.one <pistoletviktor@gmail.com>
2022-12-04 20:37:59 +0300

    task 24 first occurence_1

---
+++ seminar_4/task_24/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу которая принимает на вход число
+// а выдает сумму чисел
+// 7 ->  28   or   4 --> 10
+
+Console.WriteLine("Enter a number");
+int numA = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for(int i = 0; i <= numA;i++)
+{
+    result = result + i;
+}
+Console.WriteLine(result);
