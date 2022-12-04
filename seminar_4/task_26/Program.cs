8b67f11af2496465e31a4dc27005dda7d69505c5
Viktor St.one <pistoletviktor@gmail.com>
2022-12-05 00:43:56 +0300

    task_26 is completed

---
+++ seminar_4/task_26/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу которая принимает  
+// на вход число и выдает количество цифр в числе.
+//456 ->  3
+//  4241 --> 4
+
+
+
+int Numsearch(int num)
+{
+    int i = 0;
+    int counter = 0;
+    for(i = num;i >0; i = i/ 10)
+    {
+        counter++;
+    }
+    Console.WriteLine(counter);
+    return counter;
+}
+
+int getNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = getNumber("Введите число: ");
+Numsearch(number);
+
+
+
+
+
