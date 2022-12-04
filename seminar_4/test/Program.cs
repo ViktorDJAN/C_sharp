7b833ecd6bf719449407992e7f57294ec0836510
Viktor St.one <pistoletviktor@gmail.com>
2022-12-05 02:09:04 +0300

    как создать массив еще оин из вариантов

---
+++ seminar_4/test/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введити длину массива");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[len];
+
+
+int lenght = array.Length;
+int index = 0;
+ while(index < lenght)
+{
+    array[index] = new Random().Next(0,2);
+    index += 1;
+}
+Console.WriteLine($"[{string.Join(",",array)}]");
+
