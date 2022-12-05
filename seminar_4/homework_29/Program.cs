c6d1692ef4bc3780e59915704adb346f5ce08322
Viktor St.one <pistoletviktor@gmail.com>
2022-12-05 23:16:15 +0300

    hometask29 is completed

---
+++ seminar_4/homework_29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int [] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Enter a number");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(",", array)}]");
+    
