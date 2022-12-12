37f57eb7f01f28715724a614f6d15c35c8eec1e0
Viktor St.one <pistoletviktor@gmail.com>
2022-12-12 12:48:14 +0300

    task 45

---
+++ seminar_6/task_45/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] massive = {1,44,54,21,22};
+int [] mass = new int [massive.Length];
+for(int i = 0; i< massive.Length;i++)
+{
+    mass[i] = massive[i];
+  
+}
+Console.WriteLine(string.Join(",",mass));
