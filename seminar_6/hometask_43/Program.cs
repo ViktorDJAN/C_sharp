1d9371a899f50cd7afee2d055bd425841ed7dbd6
Viktor St.one <pistoletviktor@gmail.com>
2022-12-13 10:44:07 +0300

    hometask_43 is completed

---
+++ seminar_6/hometask_43/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1 = 2; double k1 = 5; double b2 = 4; double k2 = 9 ;
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+
+
+Console.WriteLine($"The required cross point is equal {(x,y)}");
