ecdb22a620692314f5887cb18ee22675304bc8f2
Viktor St.one <pistoletviktor@gmail.com>
2022-12-15 13:09:52 +0300

    hometask_52

---
+++ seminar_7/hometask_52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] getMatrix(int rowsCount,int columnsCount,int leftRange,int rightRange)
+{
+    int[,] matr = new int[rowsCount,columnsCount];
+    Random rand = new Random();
+    for(int i=0;i<matr.GetLength(0);i++)
+    {
+        for(int j=0;j<matr.GetLength(1);j++)
+        {
+        matr[i,j] = rand.Next(leftRange,rightRange);
+        }
+    }
+    return matr;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void printMatrix(int[,] matr)
+{
+    for(int i=0;i<matr.GetLength(0);i++)
+    {
+        for(int j=0;j<matr.GetLength(1);j++)
+        {
+            Console.Write(matr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int rows = GetNumber("Введите число строк: ");
+int columns = GetNumber("Введите число столбцов: ");
+int leftSide = GetNumber("Введите левую границу числового диапазона: ");
+int rightSide = GetNumber("Введите правую границу числового диапазона: ");
+
+int [,] newMatrix = getMatrix(rows,columns,leftSide,rightSide);
+printMatrix(newMatrix);
+Console.WriteLine();
+  
+for(int i=0;i< newMatrix.GetLength(0);i++)
+{
+    double count = 0;
+    for(int j=0;j< newMatrix.GetLength(1);j++)
+     {
+        count += newMatrix[j,i];
+        Console.WriteLine(newMatrix[j,i]);
+    }
+    Console.WriteLine($"значение суммы {count}");
+    Console.WriteLine($"среднее арифметическое = {count/newMatrix.GetLength(0)}");
+    Console.WriteLine();
+   
+}
+
+
+
+
+
