3191b0b4390a52a9b715725d9b77f341929bbd1a
Viktor St.one <pistoletviktor@gmail.com>
2022-12-17 08:32:06 +0300

    task 57

---
+++ seminar_8/task_57/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+// В нашей исходной матрице встречаются элементы от 0 до 9
+
+int[,] getMatrix(int rowsCount,int columnsCount,int leftRange,int rightRange)
+{
+    int[,] matr = new int[rowsCount,columnsCount];
+    Random rand = new Random();
+    for(int i=0;i<matr.GetLength(0);i++)
+    {
+        for(int j=0;j<matr.GetLength(1);j++)
+        {
+        matr[i,j] = rand.Next(leftRange,rightRange);
+        }
+    }
+    return matr;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void printMatrix(int[,] matr)
+{
+    for(int i=0;i<matr.GetLength(0);i++)
+    {
+        for(int j=0;j<matr.GetLength(1);j++)
+        {
+            Console.Write(matr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int []  numberSearch(int[,] matr)
+{
+    int[] res = new int [10];
+    for(int i=0;i<matr.GetLength(0);i++)
+    { 
+      for(int j=0;j<matr.GetLength(1);j++)
+      {
+        res[matr[i,j]] ++;
+      }
+    }
+    return res;
+           
+}
+ 
+    
+
+
+
+int rows = GetNumber("Введите число строк: ");
+int columns = GetNumber("Введите число столбцов: ");
+int leftSide = GetNumber("Введите левую границу числового диапазона: ");
+int rightSide = GetNumber("Введите правую границу числового диапазона: ");
+
+int [,] newMatrix = getMatrix(rows,columns,leftSide,rightSide);
+printMatrix(newMatrix);
+Console.WriteLine();
+int [] somearr = numberSearch(newMatrix);
+Console.WriteLine(string.Join(", ", somearr));
+
+
