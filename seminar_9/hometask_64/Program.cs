eef9b35bacc761fe240f1878eccd89d355ae6a6e
Viktor St.one <pistoletviktor@gmail.com>
2022-12-20 21:32:21 +0300

    hometask 64 is done

---
+++ seminar_9/hometask_64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+void numA (int num)
+{
+    if(num ==0)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(num + " ");
+        numA(num-1);
+       
+    }
+
+}
+numA(num);
