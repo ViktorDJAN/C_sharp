dfccd80b1fa2025aa81d5ed3380114e3f62eadd0
Viktor St.one <pistoletviktor@gmail.com>
2022-12-20 20:48:41 +0300

    task 67 is done

---
+++ seminar_9/task_67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int m  =456;
+int numbers(int m)
+{
+    int sum = 0;
+    if(m/10 ==0)
+    {
+        return m%10;
+    }
+    sum = numbers(m/10) + m%10;
+    return sum;
+}
+int sum = numbers(m);
+Console.WriteLine(sum);
